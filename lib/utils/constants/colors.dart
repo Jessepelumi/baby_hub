d8488b7a13d5fd74dbfb824fcc10e7be6020c6e6
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-07 10:02:08 +0100

    added linear gradient to utility colors

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -8,6 +8,17 @@ class TColors {
   static const Color secondary = Color(0xFFFFE24B);
   static const Color accent = Color(0xFFb0c7ff);
 
+  // Gradient colors
+  static const Gradient linearGradient = LinearGradient(
+    begin: Alignment(0.0, 0.0),
+    end: Alignment(0.707, -0.707),
+    colors: [
+      Color(0xFFFF9A9E),
+      Color(0xFFFAD0C4),
+      Color(0xFFFAD0C4),
+    ],
+  );
+
   // Text colors
   static const Color textPrimary = Color(0xFF333333);
   static const Color textSecondary = Color(0xFF6C757D);
