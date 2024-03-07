fe59b74cf559f9127315daaf43961b5851e8e781
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-07 16:08:42 +0100

    added utility constants

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,8 @@
+class TImageStrings {
+  // App logo
+  static const String appLogo = "assets/logo/BabyHubLogo.png";
+
+  // Social logo
+  static const String googleLogo = "assets/logo/google.png";
+  static const String facebookLogo = "assets/logo/facebook.png";
+}
