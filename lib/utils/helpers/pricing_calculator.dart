6c7375557de7c212bab7ea16b6901c3df9e47bb4
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-08 11:47:30 +0100

    added all necessary utilities

---
+++ lib/utils/helpers/pricing_calculator.dart
@@ -0,0 +1,35 @@
+class TPricingCalculator {
+  // Calculate price based on tax and shipping
+  static double calculateTotalPrice(double productPrice, String location) {
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
+
+    double shippingCost = getShippingCost(location);
+
+    double totalPrice = productPrice + taxAmount + shippingCost;
+    return totalPrice;
+  }
+
+  // Calculate shipping cost
+  static String calculateShippingCost(double productPrice, String location) {
+    double shippingCost = getShippingCost(location);
+    return shippingCost.toStringAsFixed(2);
+  }
+
+  // Calculate tax rate
+  static String calculateTax(double productPrice, String location) {
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
+    return taxAmount.toStringAsFixed(2);
+  }
+
+  static double getTaxRateForLocation(String location) {
+    // Look up the tax rate for the given location from a tax rate API
+    return 0.10;
+  }
+
+  static double getShippingCost(String location) {
+    // Lookup the shipping cost for the given location using a shipping rate API.
+    return 5.00;
+  }
+}
