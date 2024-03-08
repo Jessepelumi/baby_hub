80232bd02fbb81ed5b0f4d1cc909fbd60142ffef
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-09 00:26:05 +0100

    updated text strings file

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -12,6 +12,48 @@ class TTextStrings {
   static const String onBoardingSubTitle3 =
       "From Our Doorstep to Yours - Swift, Secure and Contactless Delivery!";
 
+  // Authentication heading text
+  static const String loginTitle = "Welcome back";
+  static const String loginSubTitle =
+      "Discover Limitless Choices and Unmatched Convenience";
+  static const String signupTitle = "Let's create your account";
+  static const String forgetPasswordTitle = "Forger password";
+  static const String forgotPasswordSubTitle =
+      "Enter your email and we will send you a password reset link";
+  static const String changeYourPasswordTitle = "Password Reset Email Sent";
+  static const String changeYourPasswordSubTitle =
+      "Your Account Security is Important to Us! We've Sent You a Secure Link to Safely Change Your Password and Keep Your Account Protected";
+  static const String confirmEmail = "Verify your email address";
+  static const String confirmEmailSubTitle =
+      "Congratulations Your Account Awaits: Verify Your Email to Start Shopping and Experience a World of Unrivaled Offers";
+  static const String emailNotReceivedMessage =
+      "Didn't get the email? Check your junk/spam folder or request a new link";
+  static const String yourAccountCreatedTitle =
+      "Your account successfully created!";
+  static const String yourAccountCreatedSubTitle =
+      "Welcome to your ultimate shopping destination for your toddlers!";
+
+  // Authentication form text
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+  static const String email = "Email address";
+  static const String password = "Password";
+  static const String newPassword = "New Password";
+  static const String username = "Username";
+  static const String phoneNo = "Phone Number";
+  static const String rememberMe = "Remember Me";
+  static const String forgotPassword = "Forgot Password?";
+  static const String signin = "Sign In";
+  static const String createAccount = "Create Account";
+  static const String orSigninWith = "or sign in with";
+  static const String orSignupWith = "or sign up with";
+  static const String iAgreeTo = "I agree to";
+  static const String privacyPolicy = "Privacy Policy";
+  static const String termsOfService = "Terms of Service";
+  static const String verificationCode = "Verification Code";
+  static const String resendEmail = "Resend Email";
+  static const String resendEmailIn = "Resend email in";
+
   // Home
   static const String homeAppBarTitle = "Good day for shopping";
   static const String homeAppBarSubTitle = "John Doe";
