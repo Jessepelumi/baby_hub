2e854bcbea180e58da19f6b396fc56f624f98474
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-14 19:51:19 +0100

    added signup to login screen

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,6 +1,8 @@
+import 'package:baby_hub/features/authentication/screens/signup/signup.dart';
 import 'package:baby_hub/utils/constants/sizes.dart';
 import 'package:baby_hub/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 class LoginForm extends StatelessWidget {
@@ -84,7 +86,7 @@ class LoginForm extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: OutlinedButton(
-                onPressed: () {},
+                onPressed: () => Get.to(() => const Signup()),
                 child: const Text(TTextStrings.createAccount),
               ),
             ),
