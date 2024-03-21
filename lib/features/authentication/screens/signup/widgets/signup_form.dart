9b83ecf527ea5cbd853b4c370003659364bd5694
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-22 00:01:31 +0100

    created form widget for signup

---
+++ lib/features/authentication/screens/signup/widgets/signup_form.dart
@@ -0,0 +1,100 @@
+import 'package:baby_hub/features/authentication/screens/signup/verify_email.dart';
+import 'package:baby_hub/features/authentication/screens/signup/widgets/t_and_c_checkbox.dart';
+import 'package:baby_hub/utils/constants/sizes.dart';
+import 'package:baby_hub/utils/constants/text_strings.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class SignupForm extends StatelessWidget {
+  const SignupForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  expands: false,
+                  decoration: const InputDecoration(
+                    labelText: TTextStrings.firstName,
+                    prefixIcon: Icon(Iconsax.user),
+                  ),
+                ),
+              ),
+              const SizedBox(width: TSizes.spaceBtwInputFields),
+              Expanded(
+                child: TextFormField(
+                  expands: false,
+                  decoration: const InputDecoration(
+                    labelText: TTextStrings.lastName,
+                    prefixIcon: Icon(Iconsax.user),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: TSizes.spaceBtwInputFields),
+
+          // Username
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: TTextStrings.username,
+              prefixIcon: Icon(Iconsax.user_edit),
+            ),
+          ),
+          const SizedBox(height: TSizes.spaceBtwInputFields),
+
+          // Email
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: TTextStrings.email,
+              prefixIcon: Icon(Iconsax.direct),
+            ),
+          ),
+          const SizedBox(height: TSizes.spaceBtwInputFields),
+
+          // Phone Number
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: TTextStrings.phoneNo,
+              prefixIcon: Icon(Iconsax.call),
+            ),
+          ),
+          const SizedBox(height: TSizes.spaceBtwInputFields),
+
+          // Password
+          TextFormField(
+            obscureText: true,
+            decoration: const InputDecoration(
+              labelText: TTextStrings.password,
+              prefixIcon: Icon(Iconsax.password_check),
+              suffixIcon: Icon(Iconsax.eye_slash),
+            ),
+          ),
+          const SizedBox(height: TSizes.spaceBtwInputFields),
+
+          // T&Cs checkbox
+          const TermsAndConditionsCheckbox(),
+          const SizedBox(height: TSizes.spaceBtwSections),
+
+          // Signup button
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () => Get.to(() => const VerifyEmailScreen()),
+              child: const Text(
+                TTextStrings.createAccount,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
