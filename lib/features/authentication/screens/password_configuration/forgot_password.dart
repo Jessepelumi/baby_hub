80677b925bceb0a3e33e7de1937d0463669ac2a5
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-22 02:09:51 +0100

    created the forgot password and reset password screens

---
+++ lib/features/authentication/screens/password_configuration/forgot_password.dart
@@ -0,0 +1,63 @@
+import 'package:baby_hub/features/authentication/screens/password_configuration/reset_password.dart';
+import 'package:baby_hub/utils/constants/sizes.dart';
+import 'package:baby_hub/utils/constants/text_strings.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class ForgotPassword extends StatelessWidget {
+  const ForgotPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () => Get.back(),
+            icon: Icon(CupertinoIcons.clear),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(TSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Headings
+            Text(
+              TTextStrings.forgetPasswordTitle,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: TSizes.spaceBtwItems),
+            Text(
+              TTextStrings.forgotPasswordSubTitle,
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections * 2),
+
+            // Textfield
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: TTextStrings.email,
+                prefixIcon: Icon(Iconsax.direct_right),
+              ),
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
+
+            // Submit button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () => Get.off(() => const ResetPassword()),
+                child: const Text(TTextStrings.submit),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
