602c1bcdbf49602018597395b8dfe1ae7f374874
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-23 17:46:32 +0100

    primary header and circular container

---
+++ lib/common/widgets/custom/container/primary_header_container.dart
@@ -0,0 +1,43 @@
+import 'package:baby_hub/common/widgets/custom/container/circular_container.dart';
+import 'package:baby_hub/common/widgets/custom/curved_edges/curved_edges_widget.dart';
+import 'package:baby_hub/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class PrimaryHeaderContainer extends StatelessWidget {
+  const PrimaryHeaderContainer({
+    super.key, required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return CurvedEdgesWidget(
+      child: Container(
+        color: TColors.primary,
+        padding: const EdgeInsets.all(0),
+        child: SizedBox(
+          height: 400,
+          child: Stack(
+            children: [
+              Positioned(
+                top: -150,
+                right: -250,
+                child: CicularContainer(
+                  backgroundColor: TColors.textWhite.withOpacity(0.1),
+                ),
+              ),
+              Positioned(
+                top: 100,
+                right: -300,
+                child: CicularContainer(
+                  backgroundColor: TColors.textWhite.withOpacity(0.1),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
