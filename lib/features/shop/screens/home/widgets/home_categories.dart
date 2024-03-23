f35e04047940f45c3b4873eabf9dca2df30c708c
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-23 19:37:22 +0100

    implemented home categories

---
+++ lib/features/shop/screens/home/widgets/home_categories.dart
@@ -0,0 +1,27 @@
+import 'package:baby_hub/common/widgets/image_text/vertical_image_text.dart';
+import 'package:flutter/material.dart';
+
+class HomeCategories extends StatelessWidget {
+  const HomeCategories({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 80,
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: 6,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (_, index) {
+          return VerticalImageText(
+            image: "",
+            title: "",
+            onTap: () {},
+          );
+        },
+      ),
+    );
+  }
+}
