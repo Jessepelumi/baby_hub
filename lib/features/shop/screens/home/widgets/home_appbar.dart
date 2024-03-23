854cbe95a6a7cc44dadd80202c6bf1b23e08f8fc
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-23 17:47:11 +0100

    custom appbar for home page

---
+++ lib/features/shop/screens/home/widgets/home_appbar.dart
@@ -0,0 +1,43 @@
+import 'package:baby_hub/common/widgets/appbar/appbar.dart';
+import 'package:baby_hub/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:baby_hub/utils/constants/colors.dart';
+import 'package:baby_hub/utils/constants/text_strings.dart';
+import 'package:flutter/material.dart';
+
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomAppBar(
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            TTextStrings.homeAppBarTitle,
+            style: Theme.of(context)
+                .textTheme
+                .labelMedium!
+                .apply(color: TColors.grey),
+          ),
+          Text(
+            TTextStrings.homeAppBarSubTitle,
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall!
+                .apply(color: TColors.white),
+          ),
+        ],
+      ),
+      actions: [
+        CounterIcon(
+          onPressed: () {},
+          iconColor: TColors.white,
+        ),
+      ],
+    );
+  }
+}
+
