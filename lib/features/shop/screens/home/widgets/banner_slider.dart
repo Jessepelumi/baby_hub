78afc1b2102d41d46ab0d43b73df2f913d447c34
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-24 20:30:03 +0100

    created a carousel of banners

---
+++ lib/features/shop/screens/home/widgets/banner_slider.dart
@@ -0,0 +1,55 @@
+import 'package:baby_hub/common/widgets/custom/container/circular_container.dart';
+import 'package:baby_hub/common/widgets/images/rounded_banner_image.dart';
+import 'package:baby_hub/features/shop/controllers/home_controller.dart';
+import 'package:baby_hub/utils/constants/colors.dart';
+import 'package:baby_hub/utils/constants/sizes.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+class BannerSlider extends StatelessWidget {
+  const BannerSlider({
+    super.key,
+    required this.banners,
+  });
+
+  final List<String> banners;
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(HomeController());
+
+    return Column(
+      children: [
+        CarouselSlider(
+          items:
+              banners.map((url) => RoundedBannerImage(imageUrl: url)).toList(),
+          options: CarouselOptions(
+            viewportFraction: 1,
+            onPageChanged: (index, _) => controller.updatePageIndicator(index),
+          ),
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems),
+        Center(
+          child: Obx(
+            () => Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                for (int i = 0; i < banners.length; i++)
+                  CicularContainer(
+                    width: 20,
+                    height: 5,
+                    margin: EdgeInsets.only(right: 10),
+                    backgroundColor: controller.carouselCurrentIndex.value == i
+                        ? TColors.primary
+                        : TColors.grey,
+                  ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
