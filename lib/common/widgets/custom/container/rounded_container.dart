5b20829598486844e713b43c548b0a5fe2bc53a7
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-25 15:02:43 +0100

    reusable rounded container

---
+++ lib/common/widgets/custom/container/rounded_container.dart
@@ -0,0 +1,43 @@
+import 'package:baby_hub/utils/constants/colors.dart';
+import 'package:baby_hub/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class RoundedContainer extends StatelessWidget {
+  const RoundedContainer(
+      {super.key,
+      this.width,
+      this.height,
+      this.radius = TSizes.cardRadiusLg,
+      this.child,
+      this.showBorder = false,
+      this.borderColor = TColors.borderPrimary,
+      this.backgroundColor = TColors.white,
+      this.padding,
+      this.margin});
+
+  final double? width;
+  final double? height;
+  final double radius;
+  final Widget? child;
+  final bool showBorder;
+  final Color borderColor;
+  final Color backgroundColor;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      margin: margin,
+      padding: padding,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(radius),
+        border: showBorder ? Border.all(color: borderColor) : null,
+      ),
+      child: child,
+    );
+  }
+}
