68902ae406f6df5a4d9ceabff4b17e7ca646e325
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-27 17:23:23 +0100

    created reusable brand card

---
+++ lib/features/shop/screens/store/widget/brand_card.dart
@@ -0,0 +1,65 @@
+import 'package:baby_hub/common/widgets/custom/container/rounded_container.dart';
+import 'package:baby_hub/common/widgets/images/circular_image.dart';
+import 'package:baby_hub/common/widgets/texts/brand_title_text_verified.dart';
+import 'package:baby_hub/utils/constants/colors.dart';
+import 'package:baby_hub/utils/constants/enums.dart';
+import 'package:baby_hub/utils/constants/image_strings.dart';
+import 'package:baby_hub/utils/constants/sizes.dart';
+import 'package:baby_hub/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class BrandCard extends StatelessWidget {
+  const BrandCard({
+    super.key,
+    required this.showBorder,
+    this.onTap,
+  });
+
+  final bool showBorder;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: RoundedContainer(
+        padding: EdgeInsets.all(TSizes.sm),
+        showBorder: showBorder,
+        backgroundColor: Colors.transparent,
+        child: Row(
+          children: [
+            // Icon
+            CircularImage(
+              isNetworkImage: false,
+              image: TImageStrings.test,
+              backgroundColor: Colors.transparent,
+              overlayColor: THelperFunctions.isDarkMode(context)
+                  ? TColors.white
+                  : TColors.black,
+            ),
+            const SizedBox(width: TSizes.spaceBtwItems / 2),
+
+            // Text
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BrandTitleTextVerified(
+                    title: "JEOLAD",
+                    brandTextSize: TextSizes.large,
+                  ),
+                  Text(
+                    "256 products",
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
