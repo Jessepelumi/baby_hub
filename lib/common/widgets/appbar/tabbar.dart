d34a2cc7aa1bf903a72ede559dfa7cfb8c022554
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-27 16:38:16 +0100

    created custom tabbar

---
+++ lib/common/widgets/appbar/tabbar.dart
@@ -0,0 +1,32 @@
+import 'package:baby_hub/utils/constants/colors.dart';
+import 'package:baby_hub/utils/device/device_utility.dart';
+import 'package:baby_hub/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class CustomTabBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomTabBar({
+    super.key,
+    required this.tabs,
+  });
+
+  final List<Widget> tabs;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Material(
+      color: dark ? TColors.black : TColors.white,
+      child: TabBar(
+        tabs: tabs,
+        isScrollable: true,
+        indicatorColor: TColors.primary,
+        labelColor: dark ? TColors.white : TColors.primary,
+        unselectedLabelColor: TColors.darkGrey,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(TDeviceUtils.getAppBarHeight());
+}
