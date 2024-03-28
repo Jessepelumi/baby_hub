f90100c37535dce3ea3aa750dd357cf8c7160fdf
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-28 15:39:53 +0100

    created brand showcase card

---
+++ lib/common/widgets/brands/brand_showcase.dart
@@ -0,0 +1,56 @@
+import 'package:baby_hub/common/widgets/brands/brand_card.dart';
+import 'package:baby_hub/common/widgets/custom/container/rounded_container.dart';
+import 'package:baby_hub/utils/constants/colors.dart';
+import 'package:baby_hub/utils/constants/sizes.dart';
+import 'package:baby_hub/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class BrandShowcase extends StatelessWidget {
+  const BrandShowcase({
+    super.key,
+    required this.images,
+  });
+
+  final List<String> images;
+
+  @override
+  Widget build(BuildContext context) {
+    return RoundedContainer(
+      showBorder: true,
+      borderColor: TColors.darkGrey,
+      backgroundColor: Colors.transparent,
+      padding: EdgeInsets.all(TSizes.md),
+      margin: EdgeInsets.only(bottom: TSizes.spaceBtwItems),
+      child: Column(
+        children: [
+          // Brand with products count
+          BrandCard(showBorder: false),
+
+          // Brand with top 3 products
+          Row(
+            children: images
+                .map((image) => brandTopProductImage(image, context))
+                .toList(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget brandTopProductImage(String image, context) {
+    return Expanded(
+      child: RoundedContainer(
+        height: 100,
+        padding: EdgeInsets.all(TSizes.md),
+        margin: EdgeInsets.only(right: TSizes.sm),
+        backgroundColor: THelperFunctions.isDarkMode(context)
+            ? TColors.darkerGrey
+            : TColors.light,
+        child: Image(
+          fit: BoxFit.contain,
+          image: AssetImage(image),
+        ),
+      ),
+    );
+  }
+}
