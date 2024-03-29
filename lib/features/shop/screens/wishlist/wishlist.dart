4740b378015aa29f5327a542c4312df25561a7b2
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-29 11:23:57 +0100

    done with wishlist structure

---
+++ lib/features/shop/screens/wishlist/wishlist.dart
@@ -0,0 +1,44 @@
+import 'package:baby_hub/common/widgets/appbar/appbar.dart';
+import 'package:baby_hub/common/widgets/icons/heart_icon.dart';
+import 'package:baby_hub/common/widgets/layout/grid_layout.dart';
+import 'package:baby_hub/common/widgets/products/product_cards/product_card_vertical.dart';
+import 'package:baby_hub/features/shop/screens/home/home.dart';
+import 'package:baby_hub/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class WishlistScreen extends StatelessWidget {
+  const WishlistScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(
+        title: Text(
+          "Wishlist",
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        actions: [
+          HeartIconWidget(
+            icon: Iconsax.add,
+            onPressed: () => Get.to(HomeScreen()),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              GridLayoutWIdget(
+                itemCount: 6,
+                itemBuilder: (_, index) => ProductCardVertical(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
