54fb9889c2397e3a9f7f1a84ec2ea011182fadec
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-29 21:32:22 +0100

    created structure for settings menu tile

---
+++ lib/common/widgets/list_tile/settings_menu_tile.dart
@@ -0,0 +1,29 @@
+import 'package:baby_hub/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class SettingMenuTile extends StatelessWidget {
+  const SettingMenuTile({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.subTitle,
+    this.trailing,
+    required this.onTap,
+  });
+
+  final IconData icon;
+  final String title, subTitle;
+  final Widget? trailing;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(icon, size: 28, color: TColors.primary),
+      title: Text(title, style: Theme.of(context).textTheme.titleMedium),
+      subtitle: Text(subTitle, style: Theme.of(context).textTheme.labelMedium),
+      trailing: trailing,
+      onTap: onTap,
+    );
+  }
+}
