a74df84b003a6dba011e3951058f373058e993c0
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-03-29 22:23:43 +0100

    modified the color of skip button in onboarding screen

--- lib/features/authentication/screens/onboarding/widgets/onboarding_skip.dart
+++ lib/features/authentication/screens/onboarding/widgets/onboarding_skip.dart
@@ -2,6 +2,7 @@ import 'package:baby_hub/features/authentication/controllers/onboarding/onboardi
 import 'package:baby_hub/utils/constants/colors.dart';
 import 'package:baby_hub/utils/constants/sizes.dart';
 import 'package:baby_hub/utils/device/device_utility.dart';
+import 'package:baby_hub/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 
 class OnBoardingSkip extends StatelessWidget {
@@ -16,10 +17,12 @@ class OnBoardingSkip extends StatelessWidget {
       right: TSizes.defaultSpace,
       child: TextButton(
         onPressed: () => OnBoardingController.instance.skipPage(),
-        child: const Text(
+        child: Text(
           "Skip",
           style: TextStyle(
-            color: TColors.primary,
+            color: THelperFunctions.isDarkMode(context)
+                ? TColors.white
+                : TColors.dark,
           ),
         ),
       ),
