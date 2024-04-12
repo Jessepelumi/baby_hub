79181ac729ae766c9ed0b7f7f8b74bb9bb18e642
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-04-12 22:58:58 +0100

    made price text color change

--- lib/common/widgets/products/product_cards/product_price_text.dart
+++ lib/common/widgets/products/product_cards/product_price_text.dart
@@ -1,3 +1,4 @@
+import 'package:baby_hub/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class ProductPriceText extends StatelessWidget {
@@ -8,11 +9,13 @@ class ProductPriceText extends StatelessWidget {
     this.maxLines = 1,
     this.isLarge = false,
     this.lineThrough = false,
+    this.isCheckout = false,
   });
 
   final String currency, price;
   final int maxLines;
   final bool isLarge;
+  final bool isCheckout;
   final bool lineThrough;
 
   @override
@@ -21,11 +24,13 @@ class ProductPriceText extends StatelessWidget {
       currency + price,
       maxLines: maxLines,
       overflow: TextOverflow.ellipsis,
-      style: isLarge
-          ? Theme.of(context).textTheme.headlineMedium!.apply(
-              decoration: lineThrough ? TextDecoration.lineThrough : null)
-          : Theme.of(context).textTheme.titleLarge!.apply(
-              decoration: lineThrough ? TextDecoration.lineThrough : null),
+      style: isCheckout
+          ? Theme.of(context).textTheme.titleLarge!.apply(color: TColors.white)
+          : isLarge
+              ? Theme.of(context).textTheme.headlineMedium!.apply(
+                  decoration: lineThrough ? TextDecoration.lineThrough : null)
+              : Theme.of(context).textTheme.titleLarge!.apply(
+                  decoration: lineThrough ? TextDecoration.lineThrough : null),
     );
   }
 }
