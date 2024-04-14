728c568ed4ff8b260519d86a1da829c60573ea8f
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-04-15 00:27:44 +0100

    created order screen

---
+++ lib/features/shop/screens/order/order.dart
@@ -0,0 +1,25 @@
+import 'package:baby_hub/common/widgets/appbar/appbar.dart';
+import 'package:baby_hub/features/shop/screens/order/widgets/order_list.dart';
+import 'package:baby_hub/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class OrderScreen extends StatelessWidget {
+  const OrderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(
+        showBackArrow: true,
+        title: Text(
+          "My Orders",
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(TSizes.defaultSpace),
+        child: OrderList(),
+      ),
+    );
+  }
+}
