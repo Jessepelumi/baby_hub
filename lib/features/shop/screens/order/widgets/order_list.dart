662c338a390c13359b208cbae81d43fcef981b1d
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-04-15 00:27:19 +0100

    created order list item

---
+++ lib/features/shop/screens/order/widgets/order_list.dart
@@ -0,0 +1,93 @@
+import 'package:baby_hub/common/widgets/custom/container/rounded_container.dart';
+import 'package:baby_hub/utils/constants/colors.dart';
+import 'package:baby_hub/utils/constants/sizes.dart';
+import 'package:baby_hub/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class OrderList extends StatelessWidget {
+  const OrderList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return ListView.separated(
+      shrinkWrap: true,
+      itemCount: 4,
+      separatorBuilder: (_, __) => const SizedBox(height: TSizes.spaceBtwItems),
+      itemBuilder: (_, index) => RoundedContainer(
+        padding: EdgeInsets.all(TSizes.md),
+        //showBorder: true,
+        backgroundColor: dark ? TColors.dark : TColors.light,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Processing",
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyLarge!
+                          .apply(color: TColors.primary, fontWeightDelta: 1),
+                    ),
+                    Text(
+                      "April 14, 2024",
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                  ],
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Iconsax.arrow_right_34),
+                ),
+              ],
+            ),
+            const SizedBox(height: TSizes.spaceBtwItems / 2),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Order",
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyLarge!
+                          .apply(color: TColors.darkGrey, fontWeightDelta: 1),
+                    ),
+                    Text(
+                      "#25612",
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Shipping Date",
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyLarge!
+                          .apply(color: TColors.darkGrey, fontWeightDelta: 1),
+                    ),
+                    Text(
+                      "April 12, 2024",
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
