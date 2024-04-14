28cd6e6d7c23e0afd0435daf7ea8fe799cca4d10
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-04-14 19:51:15 +0100

    created billing address structure for checkout screen

---
+++ lib/features/shop/screens/checkout/widgets/billing_address_section.dart
@@ -0,0 +1,26 @@
+import 'package:baby_hub/common/widgets/texts/section_heading.dart';
+import 'package:baby_hub/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class BillingAddressSection extends StatelessWidget {
+  const BillingAddressSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SectionHeading(
+          title: "Shipping Address",
+          buttonTitle: "change",
+          onPressed: () {},
+        ),
+        Text(
+          "John Penofilia",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems / 2),
+      ],
+    );
+  }
+}
