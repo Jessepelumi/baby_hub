f157985a2fef6a9b3bde9f7e8f6e61f80562a07a
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-04-14 19:50:45 +0100

    added checkout screen link to cart

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -1,11 +1,12 @@
 import 'package:baby_hub/common/widgets/appbar/appbar.dart';
-import 'package:baby_hub/common/widgets/products/cart/add_remove_button.dart';
-import 'package:baby_hub/common/widgets/products/cart/cart_item.dart';
 import 'package:baby_hub/common/widgets/products/product_cards/product_price_text.dart';
+import 'package:baby_hub/features/shop/screens/cart/widgets/cart_list_item.dart';
+import 'package:baby_hub/features/shop/screens/checkout/checkout.dart';
 import 'package:baby_hub/localization/naira_symbol.dart';
 import 'package:baby_hub/utils/constants/sizes.dart';
 //import 'package:baby_hub/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -24,30 +25,7 @@ class CartScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(TSizes.defaultSpace),
-        child: ListView.separated(
-          shrinkWrap: true,
-          separatorBuilder: (_, __) =>
-              const SizedBox(height: TSizes.spaceBtwSections),
-          itemCount: 4,
-          itemBuilder: (_, index) => Column(
-            children: [
-              const CartItemWidget(),
-              const SizedBox(height: TSizes.spaceBtwItems),
-              Row(
-                children: [
-                  const SizedBox(width: 70),
-                  // Add & remove button
-                  const ProductQuantityWidget(),
-                  const SizedBox(width: TSizes.spaceBtwItems),
-                  ProductPriceText(
-                    price: "7100",
-                    currency: getNaira(),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
+        child: CartListItem(),
       ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.symmetric(
@@ -55,7 +33,7 @@ class CartScreen extends StatelessWidget {
           vertical: TSizes.defaultSpace * 2,
         ),
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () => Get.to(() => const CheckoutScreen()),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
