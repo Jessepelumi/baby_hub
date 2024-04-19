adf22b7c87eb9cd8784aacf53c767c80b43c360c
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-04-19 22:21:07 +0100

    created horizontal product cards

--- lib/common/widgets/products/product_cards/product_card_horizontal.dart
+++ lib/common/widgets/products/product_cards/product_card_horizontal.dart
@@ -1,10 +1,145 @@
+import 'package:baby_hub/common/styles/shadows.dart';
+import 'package:baby_hub/common/widgets/custom/container/rounded_container.dart';
+import 'package:baby_hub/common/widgets/icons/heart_icon.dart';
+import 'package:baby_hub/common/widgets/images/rounded_banner_image.dart';
+import 'package:baby_hub/common/widgets/products/product_cards/product_price_text.dart';
+import 'package:baby_hub/common/widgets/texts/brand_title_text_verified.dart';
+import 'package:baby_hub/features/shop/screens/product/widgets/product_title_text.dart';
+import 'package:baby_hub/localization/naira_symbol.dart';
+import 'package:baby_hub/utils/constants/colors.dart';
+import 'package:baby_hub/utils/constants/image_strings.dart';
+import 'package:baby_hub/utils/constants/sizes.dart';
+import 'package:baby_hub/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:iconsax/iconsax.dart';
 
 class ProductCardHorizontal extends StatelessWidget {
   const ProductCardHorizontal({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Container(
+      padding: const EdgeInsets.all(1),
+      decoration: BoxDecoration(
+        boxShadow: [TShadowStyle.verticalProductShadow],
+        borderRadius: BorderRadius.circular(TSizes.productImageRadius),
+        color: dark ? TColors.darkerGrey : TColors.white,
+      ),
+      child: Row(
+        children: [
+          // Thumbnail
+          RoundedContainer(
+            height: 120,
+            padding: EdgeInsets.all(TSizes.sm),
+            backgroundColor: dark ? TColors.dark : TColors.light,
+            child: Stack(
+              children: [
+                // Thumbnail
+                RoundedBannerImage(
+                  imageUrl: TImageStrings.product_test,
+                  applyImageRadius: true,
+                ),
+
+                // Sale tag
+                Positioned(
+                  top: 12,
+                  left: 5,
+                  child: RoundedContainer(
+                    radius: TSizes.sm,
+                    backgroundColor: TColors.secondary.withOpacity(0.8),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: TSizes.sm,
+                      vertical: TSizes.xs,
+                    ),
+                    child: Text(
+                      "25%",
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelLarge!
+                          .apply(color: TColors.black),
+                    ),
+                  ),
+                ),
+
+                // Favourite icon
+                const Positioned(
+                  top: 5,
+                  right: 5,
+                  child: HeartIconWidget(
+                    icon: Iconsax.heart5,
+                    width: TSizes.iconLg,
+                    height: TSizes.iconLg,
+                  ),
+                )
+              ],
+            ),
+          ),
+
+          // Details
+          SizedBox(
+            width: 172,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: TSizes.sm,
+                left: TSizes.sm,
+              ),
+              child: Column(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ProductTitleText(
+                        title: "JEOLAD Shoes",
+                        smallSize: true,
+                      ),
+                      SizedBox(height: TSizes.spaceBtwItems),
+                      BrandTitleTextVerified(title: "JEOLAD"),
+                    ],
+                  ),
+                  Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Pricing
+                      Flexible(
+                        child: ProductPriceText(
+                          price: "7100 - 7500",
+                          currency: getNaira(),
+                        ),
+                      ),
+
+                      // Add to cart
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: TColors.dark,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(TSizes.cardRadiusMd),
+                            bottomRight:
+                                Radius.circular(TSizes.productImageRadius),
+                          ),
+                        ),
+                        child: const SizedBox(
+                          width: TSizes.iconLg * 1.2,
+                          height: TSizes.iconLg * 1.2,
+                          child: Center(
+                            child: Icon(
+                              Iconsax.add,
+                              color: TColors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
