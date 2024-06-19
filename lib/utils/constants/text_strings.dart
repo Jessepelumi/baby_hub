88c10b575dfde3ba284ea3241a3aacb526858a89
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-06-19 16:15:52 +0100

    added new string constant

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -38,6 +38,7 @@ class TTextStrings {
   static const String lastName = "Last Name";
   static const String email = "Email address";
   static const String password = "Password";
+  static const String confirmPassword = "Confirm Password";
   static const String newPassword = "New Password";
   static const String username = "Username";
   static const String phoneNo = "Phone Number";
